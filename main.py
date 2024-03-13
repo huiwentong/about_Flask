59d3f6f8488b779d49c54a7c71265cb5a636d518
huiwentong <wentong.hui@pearlstudio.com>
2024-03-13 18:28:00 +0800

    update flask

--- main.py
+++ main.py
@@ -5,12 +5,14 @@
 @EMAIL   ：wentong.hui@pearlstudio.com
 @Date    ：2024/3/13 15:48 
 '''
-from flask import Flask, url_for, render_template, request, make_response, abort, redirect
+from flask import Flask, url_for, render_template, request, make_response, abort, redirect, session
 from werkzeug.utils import secure_filename
+import secrets
 
 # url_for 是用来做测试的，测试路由中的url地址，一般搭配app.test_request_context来使用
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex() # 如果想使用session， 首先必须先生成一个secrets_key
 
 @app.route('/arg/<int:id>', methods=['GET'])
 def arg(id):
@@ -37,7 +39,7 @@ def abort_test():
 # 也可以使用这种方式 @app.get('/')
 @app.route('/', methods=['GET'])
 def index():
-    return redirect("/hello/huiwentong")
+    return redirect(url_for('hello', name='redirect'))
 
 @app.route('/redirect', methods=['GET'])
 def redirect_test():
@@ -50,6 +52,32 @@ def page_not_found(e):
 @app.route('/hello/<name>', methods=['GET'])
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+#________________以下是session使用案例________________
+
+@app.route('/index', methods=['GET'])
+def index_session():
+    if 'username' in session:
+        return 'Logged in as %s' % session['username']
+    return 'you are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index_session'))
+    return '''
+        <form method="POST">
+            <input type="text" name="username">
+            <input type="submit" value=login>
+        </form>
+    '''
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index_session'))
+
 
 if __name__ == '__main__':
     print('Python')
