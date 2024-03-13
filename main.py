7831728ec6ce23e144d6f493d54402c643621a3e
huiwentong <wentong.hui@pearlstudio.com>
2024-03-13 18:01:31 +0800

    update flask

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 @EMAIL   ：wentong.hui@pearlstudio.com
 @Date    ：2024/3/13 15:48 
 '''
-from flask import Flask, url_for, render_template, request, make_response
+from flask import Flask, url_for, render_template, request, make_response, abort
 from werkzeug.utils import secure_filename
 
 # url_for 是用来做测试的，测试路由中的url地址，一般搭配app.test_request_context来使用
@@ -30,15 +30,22 @@ def arg(id):
 def login():
     return 'login'
 
+@app.route('/abort')
+def abort_test():
+    abort(401)
+
 # 也可以使用这种方式 @app.get('/')
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World!'
+    return redirect(url_for('hello', name='huiwentong'))
 
 @app.route('/redirect', methods=['GET'])
 def redirect():
-    return redirect(url_for('/'))
+    return redirect(url_for('abort'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return make_response(render_template('page_not_found.html'), 404)
 
 @app.route('/hello/<name>', methods=['GET'])
 def hello(name=None):
@@ -51,5 +58,5 @@ if __name__ == '__main__':
         print(url_for('arg', id=2))
         print(url_for('arg', id=3))
         print(url_for('login', test='/'))
-        print(url_for('index'))
+        print(url_for('hello', name="huiwentong"))
     app.run(host="192.168.100.105", debug=True, port=24217)
